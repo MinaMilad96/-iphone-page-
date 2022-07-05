eb982e05fd081e8a13847b42f171d33b80c65cc9
Mina Milad Fahim <108484734+MinaMilad96@users.noreply.github.com>
2022-07-05 18:50:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.bgimg');
+let main = document.querySelector('.main');
+
+function phones(phone) {
+    img.src = phone;
+};
+function colors(color){
+    main.style.background = color;
+};
